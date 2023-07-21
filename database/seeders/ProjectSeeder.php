f21b45546db312ed36b73c3109966c9bf24baa16
MirkoGLostia <giggi.torre@mail.com>
2023-07-21 11:01:02 +0200

    factory and database

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -5,17 +5,17 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+use App\Models\Project;
+
+class ProjectSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this -> call([
-            ProjectSeeder :: class
-        ]);
+        Project :: factory() -> count(10) -> create();
     }
 }
